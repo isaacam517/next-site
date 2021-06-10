b9eb52138e074de71e9e4567a99e59f2c460eef1
Isaac Martiniano <isaacam517@gmail.com>
2021-06-10 15:49:45 -0300

    icon

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.css'
 export default function Header() {
     return (
         <header className={styles.container}>
-          
+        
         </header>
     )
 }
